d86bb1f712e19eafaa101dbe036ec8bf850ca5f9
许杜林 <526635387@qq.com>
2020-11-02 00:35:37 +0800

    人事管理系统源代码

--- rsglxt/welcome2.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("content-type:text/html;charset=utf-8"); 
-include("common/conn.php");
-session_start();
-error_reporting(E_ALL^E_NOTICE^E_WARNING);
-//载入页面
-require 'view/welcome2.html';
-?>
